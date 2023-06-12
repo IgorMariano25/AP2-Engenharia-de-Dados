fabd6336ea346e08a39d4d9801090b1d70363000
Felipe Castelhano <felipcastelhanogh@gmail.com>
2023-06-12 10:05:31 -0300

    Enviando arquivos AP2

---
+++ dao/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+  public Connection recuperaConexao() {
+    try {
+      String sgbd = "mysql";
+      String endereco = "localhost";
+      String bd = "agenda";
+      String usuario = "root";
+      String senha = "mysqlroot";
+
+      Connection connection = DriverManager.getConnection(
+          "jdbc:" + sgbd + "://" + endereco + "/" + bd + "?useTimezone=true&serverTimezone=UTC", usuario, senha);
+
+      return connection;
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
