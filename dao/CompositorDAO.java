d32deff8fe5bf69ddc398abeb23d6d4951dbddd9
Igor Mariano <igor-mariano@outlook.com>
2023-06-12 21:20:42 -0300

    Modificando arquivo CompositorDAO.java

--- dao/CompositorDAO.java
+++ dao/CompositorDAO.java
@@ -1,5 +1,29 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import modelo.Compositor;
+
 public class CompositorDAO {
-    
+
+        private Connection connection;
+        public CompositorDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void createSemMusica(Compositor compositor){
+        try {
+            String sql = "INSERT INTO compositor (nome) VALUES (?)";
+ 
+            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                pstm.setString(1, compositor.getNome());
+                pstm.execute(); 
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
