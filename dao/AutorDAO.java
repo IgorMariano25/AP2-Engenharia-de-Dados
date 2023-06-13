3a4bcd2d94a1155b85215c51d237ec54dc611e64
Igor Mariano <igor-mariano@outlook.com>
2023-06-13 10:09:19 -0300

    Atualizando método updateAutorSemId

--- dao/AutorDAO.java
+++ dao/AutorDAO.java
@@ -2,8 +2,11 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import modelo.Autor;
 
@@ -27,4 +30,122 @@ public class AutorDAO {
             throw new RuntimeException(e);
         }
     }
+      public Autor lerAutoresComId(Autor autor, int id) {
+        try {
+            String sql = "SELECT * FROM autor WHERE id = ?";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+                pstm.setInt(1, id);
+
+                try (ResultSet rs = pstm.executeQuery()) {
+                    if (rs.next()) {
+                        Autor leituraAutorComId = new Autor(sql);
+                        leituraAutorComId.setId(rs.getInt("id"));
+                        leituraAutorComId.setNome(rs.getString("nome"));
+                        return leituraAutorComId;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public Autor lerAutoresSemId(Autor autor) {
+        String sql = "SELECT * FROM autor WHERE nome = ?";
+
+        try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+            pstm.setString(1, autor.getNome());
+
+            try (ResultSet rs = pstm.executeQuery()) {
+                if (rs.next()) {
+                    Autor leituraAutorSemId = new Autor(rs.getString("nome"));
+                    return leituraAutorSemId;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    public void updateAutorComId(Autor autor, int id) {
+        try {
+            String sql = "UPDATE autor SET nome = ? WHERE id = ?";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+                pstm.setString(1, autor.getNome());
+                pstm.setInt(2, autor.getId());
+                pstm.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+        public void updateAutorSemId(Autor autor) {
+        try {
+            String sql = "UPDATE autor SET nome = ? WHERE nome = ?";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+                pstm.setString(1, autor.getNovoNome());
+                pstm.setString(2, autor.getNome());
+                pstm.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteComId(Autor autor, int id) {
+        try {
+            String sql = "DELETE FROM autor WHERE id = ? AND nome = ?";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+                pstm.setInt(1, id);
+                pstm.setString(1, autor.getNome());
+                pstm.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+        public void deleteSemId(Autor autor) {
+        try {
+            String sql = "DELETE FROM autor WHERE nome = ?";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+                pstm.setString(1, autor.getNome());
+                pstm.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // public List<Autor> getAll() {
+    //     try {
+    //         String sql = "SELECT * FROM autor";
+
+    //         try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+    //             try (ResultSet rs = pstm.executeQuery()) {
+    //                 List<Autor> autores = new ArrayList<>();
+
+    //                 while (rs.next()) {
+    //                     Autor autor = new Autor(sql);
+    //                     autor.setId(rs.getInt("id"));
+    //                     autor.setNome(rs.getString("nome"));
+    //                     autores.add(autor);
+    //                 }
+
+    //                 return autores;
+    //             }
+    //         }
+    //     } catch (SQLException e) {
+    //         throw new RuntimeException(e);
+    //     }
+    // }
 }
