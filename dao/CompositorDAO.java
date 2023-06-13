a554c3ce33676c49ef536bf670882530486aa598
Igor Mariano <igor-mariano@outlook.com>
2023-06-13 14:21:57 -0300

    Criando método updateCompositorSemId

--- dao/CompositorDAO.java
+++ dao/CompositorDAO.java
@@ -2,9 +2,12 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import modelo.Autor;
+import modelo.Compositor;
 import modelo.Compositor;
 
 public class CompositorDAO {
@@ -14,13 +17,48 @@ public class CompositorDAO {
         this.connection = connection;
     }
 
-    public void createSemMusica(Compositor compositor){
+    public void criarSemMusica(Compositor compositor){
         try {
             String sql = "INSERT INTO compositor (nome) VALUES (?)";
  
             try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
                 pstm.setString(1, compositor.getNome());
                 pstm.execute(); 
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // VERIFICAR COM A TALITA
+    
+    // public Compositor lerCompositorSemId(Compositor compositor) {
+    //     String sql = "SELECT * FROM compositor WHERE nome = ?";
+
+    //     try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+    //         pstm.setString(1, compositor.getNome());
+
+    //         try (ResultSet rs = pstm.executeQuery()) {
+    //             if (rs.next()) {
+    //                 Compositor lerCompositorSemId = new Compositor(rs.getString("nome"));
+    //                 return lerCompositorSemId;
+    //             }
+    //         }
+    //     } catch (SQLException e) {
+    //         throw new RuntimeException(e);
+    //     }
+
+    //     return null;
+    // }
+
+    public void updateCompositorSemId(Compositor compositor) {
+        try {
+            String sql = "UPDATE compositor SET nome = ? WHERE nome = ?";
+
+            try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+                pstm.setString(1, compositor.getNovoNome());
+                pstm.setString(2, compositor.getNome());
+                pstm.executeUpdate();
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
