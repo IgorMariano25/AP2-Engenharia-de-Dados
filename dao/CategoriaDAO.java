1a101e378f3e04bee69c96b5dff53d7817125dcd
Igor Mariano <igor-mariano@outlook.com>
2023-06-14 11:07:31 -0300

    Criando método criarSemMusica no arquivo CategoriaDAO

--- dao/CategoriaDAO.java
+++ dao/CategoriaDAO.java
@@ -1,6 +1,9 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import modelo.Categoria;
 
@@ -10,5 +13,18 @@ public class CategoriaDAO {
  
     public CategoriaDAO(Connection connection) {
         this.connection = connection;
+    }
+
+        public void criarSemMusica(Categoria categoria){
+        try {
+            String sql = "INSERT INTO categoria (nome) VALUES (?)";
+ 
+            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                pstm.setString(1, categoria.getNome());
+                pstm.execute(); 
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
