89447673b42bc580f88d98974a0f2fbaa08deeca
Igor Mariano <igor-mariano@outlook.com>
2023-06-14 10:59:55 -0300

    Criando método para adicionar playlist a uma categoria

--- modelo/Categoria.java
+++ modelo/Categoria.java
@@ -10,18 +10,15 @@ public class Categoria {
     this.nome = nome;
     this.playlists = new ArrayList<Playlist>();
   }
-  
+
   public String getNome() {
     return nome;
   }
   public void setNome(String nome) {
     this.nome = nome;
   }
-  public int getFkMusica() {
-    return fkMusica;
-  }
-  public void setFkMusica(int fkMusica) {
-    this.fkMusica = fkMusica;
-  }
 
+  public void addPlaylist(Playlist playlist) {
+    this.playlists.add(playlist);
+  }
 }
