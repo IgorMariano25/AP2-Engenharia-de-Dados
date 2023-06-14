8784340afaa82d51d7578132de18ede1eba80078
Igor Mariano <igor-mariano@outlook.com>
2023-06-14 11:37:27 -0300

    Criando arquivo PlaylistDAO.java

--- dao/PlaylistDAO.java
+++ dao/PlaylistDAO.java
@@ -1,5 +1,12 @@
 package dao;
+import java.sql.Connection;
+
+import modelo.Playlist;
 
 public class PlaylistDAO {
-    
+        private Connection connection;
+ 
+    public PlaylistDAO(Connection connection) {
+        this.connection = connection;
+    }
 }
