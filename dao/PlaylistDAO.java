6383f9f25690ab6deb165785c9e8c7ffb492b592
Igor Mariano <igor-mariano@outlook.com>
2023-06-14 13:55:49 -0300

    Criando método criarComMusica

--- dao/PlaylistDAO.java
+++ dao/PlaylistDAO.java
@@ -9,4 +9,19 @@ public class PlaylistDAO {
     public PlaylistDAO(Connection connection) {
         this.connection = connection;
     }
+
+        public void criarComMusica(Playlist playlist, Musica musica){
+        try {
+            String sql = "INSERT INTO Playlist (dataCriacao, visbilidade, titulo) " +
+            "VALUES (?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                pstm.setDate(2, (Date) playlist.getDataCriacao());
+                pstm.setBoolean(3, playlist.getVisibilidade());
+                pstm.setString(4, playlist.getTitulo());
+                pstm.execute(); 
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
